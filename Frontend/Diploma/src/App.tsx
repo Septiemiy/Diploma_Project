d83e2db930c7f41fbf7c729f237ef91422104480
Septiemiy <pachenkomih@gmail.com>
2026-03-30 02:31:37 +0300

    Make header and hero image

--- Frontend/Diploma/src/App.tsx
+++ Frontend/Diploma/src/App.tsx
@@ -1,8 +1,15 @@
-function App() {
+import { Routes, Route } from 'react-router-dom'
+import  Landing  from './pages/Landing/Landing.tsx'
+
+import './assests/styles/normalize.css'
+
+const App = () => {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={ <Landing /> } />
+      </Routes>
     </>
   )
 }
---
+++ Frontend/Diploma/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <p className={styles.header_logo}>VidPriority</p> 
+    </div>
+  )
+}
+
+export default Header
